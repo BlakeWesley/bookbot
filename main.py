3a1280ebe2f6688f2cd05f27e905f9e82dcdb435
BlakeWesley <blakeawesley@gmail.com>
2025-09-05 08:11:55 +1000

    Adjusted to allow for a book parameter to be passed when running script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words
+import sys
+from stats import count_words, count_characters, sorted_dicts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,9 +7,27 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    filepath = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     file_contents = get_book_text(filepath)
+
+    # Get our analysis
     number_of_words = count_words(file_contents)
-    print(f"{number_of_words} words found in the document")
+    chars_dict = count_characters(file_contents)
+    sorted_char_list = sorted_dicts(chars_dict)
+
+    # Format the report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+    for pair in sorted_char_list:
+        if pair["char"].isalpha():
+            print(f"{pair["char"]}: {pair["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,37 @@
 # Counts how many words are in the string
+# Returns Integer
 def count_words(string):
     words = string.split()
     number_of_words = len(words)
     return number_of_words
+
+# Returns a dictionary of the number of times each character (including spaces and symbols) appears in a string
+# Dictionary of String -> Integer
+def count_characters(string):
+    characters = {}
+
+    for word in string:
+        for char in word:
+            char = char.lower()
+            if char not in characters:
+                characters[char] = 0
+            characters[char] += 1
+
+    return characters
+
+# Helper function for sorting by num
+def sort_on(items):
+    return items["num"]
+
+# Returns a sorted list of dictionaries
+def sorted_dicts(character_dict):
+    # We need to first get our dictionary into a list of dictionaries.
+    char_list = []
+    for char in character_dict:
+        new_dict = {"char": char, "num": character_dict[char]}
+        char_list.append(new_dict)
+
+    # Now, we need to sort our list
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
